c69f33f877f557409242e6b6fca3026ae02f4c6f
kwajwooteng <kingsley.oteng@gmail.com>
2016-06-28 04:13:37 +0000

    Commit number two
    1. Created an audio player class which treats the tracks as objects
    2. In the main audio player class included attributes to loop or play the file once
    3. Made most of the attributes in both classes private

--- MusicMixer.java
+++ MusicMixer.java
@@ -20,83 +20,183 @@ import sun.audio.ContinuousAudioDataStream;
 
 class music_line_object extends Thread 
 {
-    // provide the line object a name
-    // line object parameters
-    String name_of_line; 
-    String credits;
-    AudioStream audioStream;
-    AudioData musicData;
-    AudioPlayer musicPlayer = AudioPlayer.player;
-    ContinuousAudioDataStream loops = null;
-    InputStream in;
-    
-    
-            
-    music_line_object() {
-        this.loop = true;
-        this.name_of_line = "no_name";
-    };
-    
-    music_line_object(String input_file_name) {
-        try 
-            {
-                this.in = new FileInputStream(input_file_name);
-                this.audioStream = new AudioStream(in);
-                musicData = this.audioStream.getData();
-                loops = new ContinuousAudioDataStream(musicData);
-                musicPlayer.start(loops);
+                // provide the line object a name
+                // line object parameters
+                String name_of_line; 
+                String credits;
+                String file_name;
+                AudioStream audioStream;
+                AudioData musicData;
+                AudioPlayer musicPlayer;
+                ContinuousAudioDataStream loops;
+                AudioDataStream single;
+                InputStream in;
+                byte bytedata;
 
-                  
-            } 
-        catch (Exception e) {};
-            
-        
-        
-    };
-    
-    
-    // volume range 0 to 1
-    double gain;
-    
-    // length of the clip in microseconds. i.e. 1,000,000 = 1 second
-    double length;
-    
-    // range 1 (left) to -1 (right)
-    double pitch;
-    
-    // range 1 (left) to -1 (right)
-    double pan;
-    
-    // whether to loop clip
-    boolean loop;
-    
-    // start time and end time
-    double start_time;
-    double end_time;   
+                music_line_object() 
+                    { 
+                        this.name_of_line = "no_name";
+                        this.loops = null;
+                        this.single = null;
+                        this.in = null;
+                        this.musicPlayer = AudioPlayer.player;
+                    };
+
+                music_line_object(String input_file_name) throws java.io.IOException {
+
+                            this.in = new FileInputStream(input_file_name);
+                            this.audioStream = new AudioStream(in);
+                            musicData = this.audioStream.getData();
+                            loops = null;
+                            loops = new ContinuousAudioDataStream(musicData);
+                            single = new AudioDataStream(musicData);
+
+                            if (this.loop) 
+                            {
+                                musicPlayer.start(loops);  
+                            } 
+                                else 
+                            { 
+                                musicPlayer.start(single);      
+                            }         
+
+                };
+
+
+
+                void start_play()
+                  {
+
+                    loops = new ContinuousAudioDataStream(musicData);
+                    single = new AudioDataStream(musicData);
+
+                    if (this.loop) 
+                        {
+                            musicPlayer.start(loops);  
+                        } 
+                    else 
+                        { 
+                            musicPlayer.start(single);     
+                        }
+
+
+                    musicPlayer.start(this.audioStream);
+
+                  };
+
+                void load_track(String input_file_name) throws java.io.IOException
+                  {
+                    this.in = new FileInputStream(input_file_name);
+                    this.audioStream = new AudioStream(in);
+                    musicData = this.audioStream.getData();
+                  };
+
+                void set_loop(Boolean loop_status) throws java.io.IOException
+                  {
+                    this.loop = loop_status;
+                  };
+
+
+                // volume range 0 to 1
+                double gain;
+
+                // length of the clip in microseconds. i.e. 1,000,000 = 1 second
+                double length;
+
+                // range 1 (left) to -1 (right)
+                double pitch;
+
+                // range 1 (left) to -1 (right)
+                double pan;
+
+                // whether to loop clip
+                boolean loop;
+
+                // start time and end time
+                double start_time;
+                double end_time;   
 }
 
 class music_clip_object extends music_line_object 
-{
-
-}
+{}
 
 class audio_player_class 
-    {
-                AudioData musicData;
-                ContinuousAudioDataStream loops = null;
-                InputStream in;
+    {           
                 
                 // constructors
-                audio_player_class()  {};
-                audio_player_class(music_line_object audio_link)  { AudioPlayer.player.start(audio_link.audioStream); };
-
+                audio_player_class()  
+                {
+                    loop_sample = null;
+                    single_play_sample = null;
+                    musicPlayer = null;
+                    //this.setPlayerParameters();
+                };
+                
+                audio_player_class(music_line_object object_file)  
+                { 
+                    try 
+                        {
+                            this.set_insert(object_file);
+                            this.play();
+                        }
+                            catch (IOException io)
+                        {
+                    
+                        }
+                };
+                
+                
+                void set_insert(music_line_object object_file)
+                {
+                    // set the sound object
+                    this.casette = object_file;
+                    this.loop_track_status = casette.loop;
+                };
+                
                 // method plays line based on current settings
-                void play(music_line_object audio_link)
-                    {   
-                       
-                        AudioPlayer.player.start(audio_link.audioStream);
-                     
-                    };
+                void play() throws java.io.IOException
+                {         
+                    // generate sound samples
+                    this.loop_sample = new ContinuousAudioDataStream(this.casette.musicData);
+                    this.single_play_sample = new AudioDataStream(this.casette.musicData);
+                    this.musicPlayer = AudioPlayer.player;
+                    
+                    // loop or simply play
+                    if (this.loop_track_status) 
+                        {
+                            this.musicPlayer.start(this.loop_sample);  
+                        } 
+                    else 
+                        { 
+                            this.musicPlayer.start(this.single_play_sample);     
+                        }  
+ 
+                };
+     
+    
+                // volume range 0 to 1
+                private double gain;
+    
+                // length of the clip in microseconds. i.e. 1,000,000 = 1 second
+                private double length;
+    
+                // range 1 (left) to -1 (right)
+                private  double pitch;
+    
+                // range 1 (left) to -1 (right)
+                private double pan;
+    
+                // start time and end time
+                private double start_time;
+                private double end_time;  
+    
+                // AudioDataStream and AudioPlayer class declarations
+                private ContinuousAudioDataStream loop_sample;
+                private AudioDataStream single_play_sample;
+                private AudioPlayer musicPlayer;
+                private Boolean loop_track_status;
+                private music_line_object casette;
+       
     };
 
 public class MusicMixer {
@@ -104,44 +204,33 @@ public class MusicMixer {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
     
-       
-    // play the audio clip with the audioplayer class
-    
-    
-        // create a device to play sound
-        // defining own object will allow additional features like loop
-        audio_player_class device_a = new audio_player_class();
-        
-        // sound sample 1
-        // TODO code application logic here
-        music_line_object sound_1;
-        sound_1 = new music_line_object("/Users/Usered/Desktop/a_team_intro.wav");
-        sound_1.name_of_line = "Sheep blee";
-        sound_1.credits = "http://download.wavetlan.com/SVV/Media/HTTP/WAV/Media-Convert/Media-Convert_test5_PCM_Stereo_VBR_8SS_44100Hz.wav";
-        sound_1.loop = true;
-        
-        music_line_object sound_2;
-        sound_2 = new music_line_object("/Users/Usered/Desktop/gwh1.wav");
-        sound_2.name_of_line = "drum";
-        sound_2.credits = "http://freewavesamples.com/files/High-Conga-1.wav";
-        sound_2.loop = true;
-        
-        music_line_object sound_3;
-        sound_3 = new music_line_object("/Users/Usered/Desktop/dt_16bars_102rap.wav");
-        sound_3.name_of_line = "drum";
-        sound_3.credits = "http://www.wavlist.com/soundfx/027/";
-        sound_3.loop = true;
-        
-        music_line_object sound_4;
-        sound_4 = new music_line_object("/Users/Usered/Desktop/a_team_intro.wav");
-        sound_4.name_of_line = "drum";
-        sound_4.credits = "http://www.wavlist.com/soundfx/027/";
-        sound_4.loop = true;
-        
-        // play music
-        device_a.play(sound_3);
+                // play the audio clip with the audioplayer class
+                // create a device to play sound
+                // defining own object will allow additional features like loop
+                audio_player_class device_a;
+
+                // create sound sample 1
+                music_line_object track_1 = new music_line_object();
+                track_1.loop = true;
+                track_1.file_name = "/Users/Usered/Desktop/gwh1.wav";
+                track_1.name_of_line = "drum";
+                track_1.credits = "http://www.wavlist.com/soundfx/027/";
+                track_1.load_track("/Users/Usered/Desktop/gwh1.wav");
+                //sound_3.start_play();
+
+                // create sound sample 2
+                music_line_object track_2 = new music_line_object();
+                track_2.loop = true;
+                track_2.file_name = "/Users/Usered/Desktop/fanfare.wav";
+                track_2.credits = "http://www.wavlist.com/soundfx/027/";  
+                track_2.load_track("/Users/Usered/Desktop/fanfare.wav");
+                //sound_4.start_play();
+
+                // play music
+                device_a = new audio_player_class(track_1);
+                device_a = new audio_player_class(track_2);
        
     }
     
